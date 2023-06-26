6a79f2fd46c608faa9aaacdd357f407f39d7ddc5
alaasw1 <alaa.sweed1@gmail.com>
2023-06-27 02:14:56 +0300

    after 20

--- Electronic-Store-Frontend/src/app/components/pages/order-track-page/order-track-page.component.ts
+++ Electronic-Store-Frontend/src/app/components/pages/order-track-page/order-track-page.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Order } from 'src/app/models/order';
+import { OrderService } from 'src/app/services/order.service';
 
 @Component({
   selector: 'app-order-track-page',
@@ -6,5 +9,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./order-track-page.component.css']
 })
 export class OrderTrackPageComponent {
+  order!:Order;
+  constructor(activatedRoute: ActivatedRoute,
+              orderService:OrderService) {
+     const params = activatedRoute.snapshot.params;
+     if(!params.orderId) return;
 
+     orderService.trackOrderById(params.orderId).subscribe(order => {
+       this.order = order;
+     })
+
+  }
 }
