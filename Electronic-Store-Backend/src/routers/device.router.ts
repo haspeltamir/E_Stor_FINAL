9866d359afdf0341b288d49191695dc52e20bdbe
tamir haspel <haspeltamir@gmail.com>
2023-06-26 12:35:48 +0300

    register

--- Electronic-Store-Backend/src/routers/device.router.ts
+++ Electronic-Store-Backend/src/routers/device.router.ts
@@ -91,12 +91,4 @@ router.get(
   })
 );
 
-// router.get(
-//   "/:deviceID",
-//   asyncHandler(async (request, response) => {
-//     const device = await DeviceModel.find({ id: request.params.deviceID });
-//     response.send(device);
-//   })
-// );
-
 export default router;
